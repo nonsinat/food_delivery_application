9e01a1ad46112903ab036ab0371f593f031bee71
NonSinat <sinat.non@student.niptict.edu.kh>
2021-10-06 15:40:31 +0700

    finished Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery_admin_panel/screens/splash_screen.dart';
 
 void main() {
-
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
-    
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
       home: SplashScreen(),
     );
   }
 }
-
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:food_delivery_admin_panel/constant.dart/colors/colors.dart';
+import 'package:food_delivery_admin_panel/screens/homepage/home_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -10,10 +13,35 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 10),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePage(),
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primaryColor,
-      
+      body: SafeArea(
+        child: Center(
+          child: Text(
+            "VIGE",
+            style: Theme.of(context).textTheme.headline4!.copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 60,
+                ),
+          ),
+        ),
+      ),
     );
   }
 }
